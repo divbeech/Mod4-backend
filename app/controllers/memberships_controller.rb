e20ad37516eace10002d135eea3e07498634df5d
divbeech <dyadav1006@gmail.com>
2019-12-01 16:54:45 -0500

    Relations updated

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,4 @@
+class MembershipsController < ApplicationController
+    belongs_to :user
+    belongs_to :gym
+end
---
+++ app/controllers/user_classes_controller.rb
@@ -0,0 +1,5 @@
+class UserClassesController < ApplicationController
+    belongs_to :user
+    belongs_to :gym_class
+    
+end
